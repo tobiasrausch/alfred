63ec56178e733c4f639fd277cb625c0b6127cd51
tobiasrausch <rauschtobi@gmail.com>
2017-02-21 11:14:41 +0100

    added header for source files

--- src/util.h
+++ src/util.h
@@ -1,3 +1,26 @@
+/*
+============================================================================
+Alfred: BAM alignment statistics
+============================================================================
+Copyright (C) 2017 Tobias Rausch
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+============================================================================
+Contact: Tobias Rausch (rausch@embl.de)
+============================================================================
+*/
+
 #ifndef UTIL_H
 #define UTIL_H
 
