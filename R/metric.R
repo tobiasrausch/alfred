f371530ad90837457b55d573d3c57f990c717547
tobiasrausch <rauschtobi@gmail.com>
2017-07-06 10:33:01 +0200

    textplot

--- R/metric.R
+++ R/metric.R
@@ -3,7 +3,7 @@ library(gplots)
 args=commandArgs(trailingOnly=TRUE)
 x = read.table(args[1], header=F, comment.char="$")
 
-pdf(paste0(args[1], ".pdf"), width=8, height=4)
+pdf(paste0(args[1], ".pdf"), width=8, height=8)
 textplot(t(x), show.rownames=F, show.colnames=F)
 dev.off()
 print(warnings())
