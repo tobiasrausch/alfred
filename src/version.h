62f33126ea39ad2046264c16001977043968030a
tobiasrausch <rauschtobi@gmail.com>
2017-09-11 16:04:18 +0200

    updated to v0.1.2

--- src/version.h
+++ src/version.h
@@ -27,7 +27,7 @@ Contact: Tobias Rausch (rausch@embl.de)
 namespace bamstats
 {
 
-  std::string alfredVersionNumber = "0.1.1";
+  std::string alfredVersionNumber = "0.1.2";
 
   inline 
     void printTitle(std::string const& title) 
