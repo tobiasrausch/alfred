94a2fd5149faefcb26f422fd8b446af22fa4b11f
tobiasrausch <rauschtobi@gmail.com>
2018-01-23 14:17:15 +0100

    added GRCh38

--- gtf/downloadGTF.sh
+++ gtf/downloadGTF.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 wget 'ftp://ftp.ensembl.org/pub/release-75/gtf/homo_sapiens/Homo_sapiens.GRCh37.75.gtf.gz'
+wget 'ftp://ftp.ensembl.org/pub/release-91/gff3/homo_sapiens/Homo_sapiens.GRCh38.91.gff3.gz'
+wget 'ftp://ftp.ensembl.org/pub/release-91/gtf/homo_sapiens/Homo_sapiens.GRCh38.91.gtf.gz'
