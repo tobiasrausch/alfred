367661357afdb1f34b19b4993d9c2d732134a1de
tobiasrausch <rauschtobi@gmail.com>
2018-07-31 20:49:29 +0200

    alfred v0.1.8

--- src/version.h
+++ src/version.h
@@ -27,7 +27,7 @@ Contact: Tobias Rausch (rausch@embl.de)
 namespace bamstats
 {
 
-  std::string alfredVersionNumber = "0.1.7";
+  std::string alfredVersionNumber = "0.1.8";
 
   inline 
     void printTitle(std::string const& title) 
