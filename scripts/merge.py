ae2157c02410f5d67c7696953055596e67903933
Markus Hsi-Yang Fritz <markus.h.y.fritz@gmail.com>
2018-08-30 08:53:35 +0200

    first stab at proper merging of summary stats

--- scripts/merge.py
+++ scripts/merge.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
 import gzip
+from itertools import chain
 import json
 import sys
 
@@ -13,9 +14,35 @@ def opn(fn):
     return gzip.open(fn)
   return open(fn)
 
+def unique(iterable):
+  ret = []
+  for value in iterable:
+    if value not in ret:
+      ret.append(value)
+  return ret
+
 for file_name in sys.argv[1:]:
   with opn(file_name) as f:
     file_content = json.load(f)
   ret["samples"].extend(file_content["samples"])
 
+summary_columns = unique(
+  chain(
+    *[s["summary"]["data"]["columns"] for s in ret["samples"]]
+  )
+)
+
+for sample in ret["samples"]:
+  old_columns = sample["summary"]["data"]["columns"]
+  old_rows = [
+    dict(zip(old_columns, row)) for row in sample["summary"]["data"]["rows"]
+  ]
+  sample["summary"]["data"]["columns"] = summary_columns
+  sample["summary"]["data"]["rows"] = []
+  for row in old_rows:
+    new_row = []
+    for column in summary_columns:
+      new_row.append(row.get(column))
+    sample["summary"]["data"]["rows"].append(new_row)
+  
 print(json.dumps(ret))
