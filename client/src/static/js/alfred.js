a798eade4d57b0d97ec092002fae33b0bcda1107
Markus Hsi-Yang Fritz <markus.h.y.fritz@gmail.com>
2018-09-04 19:55:28 +0200

    client: filterable selects everywhere

--- client/src/static/js/alfred.js
+++ client/src/static/js/alfred.js
@@ -35,7 +35,7 @@ selectExamples.addEventListener('change', event => {
   showExample(event.target.value)
 })
 
-const exampleChoices = new Choices(selectExamples)
+const exampleChoices = new Choices(selectExamples, { shouldSort: false })
 
 let data, exampleData, readGroups, summary
 
@@ -44,9 +44,9 @@ const chartsContainer = document.getElementById('charts-container')
 const resultContainer = document.getElementById('result-container')
 const resultInfo = document.getElementById('result-info')
 const resultError = document.getElementById('result-error')
-const selectSample = document.getElementById('select-sample')
-const selectReadGroup = document.getElementById('select-rg')
-const selectToc = document.getElementById('select-toc')
+const selectSample = new Choices(document.getElementById('select-sample'), { shouldSort: false })
+const selectReadGroup = new Choices(document.getElementById('select-rg'), { shouldSort: false })
+const selectToc = new Choices(document.getElementById('select-toc'), { shouldSort: false })
 const summaryTab = document.getElementById('summary-tab')
 
 const fileUpload = FilePond.create(inputFile)
@@ -156,11 +156,23 @@ function handleSuccess(data) {
     readGroups[sample.id] = sample.readGroups.map(rg => rg.id)
   })
 
-  selectSample.innerHTML = samples.map(s => `<option>${s}</option>`).join('')
+  selectSample.setChoices(
+    samples.map((s, i) => ({
+      value: s,
+      label: s,
+      selected: i === 0
+    })),
+    'value',
+    'label',
+    true
+  )
 
-  selectReadGroup.innerHTML = readGroups[samples[0]]
-    .map(rg => `<option>${rg}</option>`)
-    .join('')
+  selectReadGroup.setChoices(
+    readGroups[samples[0]].map((rg, i) => ({ value: rg, label: rg, selected: i === 0 })),
+    'value',
+    'label',
+    true
+  )
 
   summary = {
     title: data.samples[0].summary.title,
@@ -203,15 +215,24 @@ function populateToc(sample, readGroup) {
     .find(s => s.id === sample)
     .readGroups.find(rg => rg.id === readGroup)
 
-  selectToc.innerHTML = `${dataRg.metrics.map(
-    metric => `<option value="${metric.id}">${metric.title}</option>`
-  )}`
+  selectToc.setChoices(
+    dataRg.metrics.map((metric, i) => ({
+      value: metric.id,
+      label: metric.title,
+      selected: i === 0
+    })),
+    'value',
+    'label',
+    true
+  )
 }
 
 window.handleTocChange = handleTocChange
 function handleTocChange() {
-  const id = selectToc.value
-  document.getElementById(id).scrollIntoView()
+  const targetId = selectToc.getValue(true)
+  setTimeout(() => {
+    document.getElementById(targetId).scrollIntoView()
+  }, 0)
 }
 
 const chartDispatch = {
