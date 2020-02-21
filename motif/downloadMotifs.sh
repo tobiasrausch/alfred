c487dd8ff423d36781fe1c92ac75173d797b22f5
tobiasrausch <rauschtobi@gmail.com>
2020-02-21 15:15:53 +0100

    jaspar 2020

--- motif/downloadMotifs.sh
+++ motif/downloadMotifs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl --output jaspar.zip 'http://jaspar.genereg.net/download/CORE/JASPAR2018_CORE_vertebrates_non-redundant_pfms_jaspar.zip'
+curl --output jaspar.zip 'http://jaspar.genereg.net/download/CORE/JASPAR2020_CORE_vertebrates_non-redundant_pfms_jaspar.zip'
 unzip jaspar.zip
 rm jaspar.zip
 cat *.jaspar | gzip -c > jaspar.gz
