873f6c48e71eae09e0ae24238e5fabb98135922f
Tobias Rausch <rauschtobi@gmail.com>
2022-09-27 11:46:32 +0200

    GTFs

--- gtf/downloadGTF.sh
+++ gtf/downloadGTF.sh
@@ -2,7 +2,7 @@
 
 wget ftp://ftp.ensembl.org/pub/grch37/current/gff3/homo_sapiens/Homo_sapiens.GRCh37.[0-9][0-9].gff3.gz
 wget ftp://ftp.ensembl.org/pub/grch37/current/gtf/homo_sapiens/Homo_sapiens.GRCh37.[0-9][0-9].gtf.gz
-wget ftp://ftp.ensembl.org/pub/current_gff3/homo_sapiens/Homo_sapiens.GRCh38.[0-9][0-9].gff3.gz
-wget ftp://ftp.ensembl.org/pub/current_gtf/homo_sapiens/Homo_sapiens.GRCh38.[0-9][0-9].gtf.gz
+wget ftp://ftp.ensembl.org/pub/current_gff3/homo_sapiens/Homo_sapiens.GRCh38.[0-9][0-9][0-9].gff3.gz
+wget ftp://ftp.ensembl.org/pub/current_gtf/homo_sapiens/Homo_sapiens.GRCh38.[0-9][0-9][0-9].gtf.gz
 
 
