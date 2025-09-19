81f988d59f6db8c6b9b6e051823d4aae3983c2db
Tobias Rausch <rauschtobi@gmail.com>
2025-09-19 13:03:54 +0200

    updated vue

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,20 +1,26 @@
-module.exports = {
+import { defineUserConfig } from '@vuepress/cli'
+import { viteBundler } from '@vuepress/bundler-vite'
+
+export default defineUserConfig({
   title: "Alfred documentation",
-  description:
-    "Documentation of Alfred, an app for BAM alignment statistics, feature counting and annotation",
+  description: "Documentation of Alfred, an app for BAM alignment statistics, feature counting and annotation",
   base: "/docs/alfred/",
+  theme: "@vuepress/theme-default",  
   themeConfig: {
     repo: "tobiasrausch/alfred",
-    nav: [
+    navbar: [
       { text: "Home", link: "/" },
       { text: "Installation", link: "/installation/" },
       { text: "Usage", link: "/cli/" },
       { text: "Web App", link: "/webapp/" },
       { text: "FAQ", link: "/faq/" }
     ],
-    sidebar: ["/installation/", "/cli/", "/webapp/", "/faq/"]
+    sidebar: [
+      { text: "Installation", link: "/installation/" },
+      { text: "Usage", link: "/cli/" },	
+      { text: "Web App", link: "/webapp/" },
+      { text: "FAQ", link: "/faq/" }
+    ]
   },
-  plugins: {
-    "@vuepress/back-to-top": true
-  }
-};
+  bundler: viteBundler({})
+})
